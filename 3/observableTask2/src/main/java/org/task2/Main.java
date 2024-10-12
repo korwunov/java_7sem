25e550837cfef63a773de825013881c0d181180b
p.korshunov <korshunov.petr@gmail.com>
2024-10-12 12:49:11 +0300

    fix for task2 for pr3

--- 3/observableTask2/src/main/java/org/task2/Main.java
+++ 3/observableTask2/src/main/java/org/task2/Main.java
@@ -8,12 +8,12 @@ import io.reactivex.Observable;
 public class Main {
     public static void main(String[] args) {
         Observable<Integer> list = Observable.range(0, 1000).filter(number -> number > 500);
-        //list.forEach(System.out::println);
+        list.forEach(System.out::println);
 
         Observable<Integer> list1 = Observable.range(0, 1000);
         Observable<Integer> list2 = Observable.range(0, 1000);
         Observable<Integer> mergedLists = Observable.concat(list1, list2);
-        //mergedLists.forEach(System.out::println);
+        mergedLists.forEach(System.out::println);
 
         list = Observable.range(0, 10);
         Observable<Integer> result = list.take(5);
